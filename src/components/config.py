4300f69a9bfd65068bc2cd0d446a4ea12f904092
ammacapagal <73205874+angelmacm@users.noreply.github.com>
2024-07-05 17:18:12 +0800

    create config.py

---
+++ src/components/config.py
@@ -0,0 +1,9 @@
+from configparser import ConfigParser
+
+baseConfig = ConfigParser()
+baseConfig.read("config.ini")
+
+coinsConfig = baseConfig['COINS']
+botConfig = baseConfig['BOT']
+xrplConfig = baseConfig['XRPL']
+dbConfig = baseConfig['DATABASE']
