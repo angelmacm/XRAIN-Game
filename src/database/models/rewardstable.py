2e927255fa5b03465ebfb45c85543bf66969d4e1
ammacapagal <73205874+angelmacm@users.noreply.github.com>
2024-07-05 17:22:40 +0800

    setup RewardsTable model

---
+++ src/database/models/rewardstable.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, DateTime, VARCHAR
+from sqlalchemy.orm import column_property
+from sqlalchemy.ext.declarative import declarative_base
+
+from components.config import dbConfig
+
+# Define the base class
+Base = declarative_base()
+
+class RewardsTable(Base):
+    __tablename__ = dbConfig['rewards_table_name']
+
+    xrpId = Column(VARCHAR, primary_key=True)
+    OGReputationRewards = Column(Integer)
+    birdReputationRewards = Column(Integer)
+    totalReputationRewards = Column(Integer)
+    traits_2DRewards = column_property(Column('2DTraitsRewards', Integer))
+    traits_3DCollabRewards = column_property(Column('3DCollabRewards', Integer))
+    totalTraits3DRewards = column_property(Column('TotalTrait3DRewards', Integer))
+    activeSells = Column(Integer)
+    penaltyPercent = Column(Integer)
+    penaltyReputationRewards = Column(Integer)
+    bonusXrainFlag = Column(Integer)
+    dailyBonusFlagDate = Column(DateTime)
+    penaltyTraits3DRewards = Column(Integer)
+    reputationFlag = Column(Integer)
+    finalRepTraitRewards = column_property(Column('FINALRepTraitRewards', Integer))
