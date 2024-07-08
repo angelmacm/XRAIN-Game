8de939336e7af4649a3ee0427186d0e3446f7c33
ammacapagal <73205874+angelmacm@users.noreply.github.com>
2024-07-08 19:55:45 +0800

    add createXrainPaymentRequest in xumm SDK

--- src/components/xummClient.py
+++ src/components/xummClient.py
@@ -16,3 +16,32 @@ class XummClient:
     def checkStatus(self, uuid):
         status = self.xummSdk.payload.get(uuid)
         return status.response.to_dict()
+    
+    def createXrainPaymentRequest(self, recipient: str, amount: float, coinHex:str = "XRP"):
+        txJson = {
+            "TransactionType": "Payment",
+            "Destination": recipient,
+        }
+        
+        if coinHex.upper() == "XRP":
+            txJson['Amount'] = str(amount * 1000000)
+        else:
+            txJson['Amount'] = {
+                "currency": coinHex,
+                "value": str(amount),
+                "issuer": 'rh3tLHbXwZsp7eciw2Qp8g7bN9RnyGa2pF'
+            }
+            
+        paymentRequest = self.xummSdk.payload.create(payload={
+            'txjson': txJson,
+            'options':{
+                "force_network": "TESTNET",
+                "expire": 30,
+                'submit': True
+            },
+            "custom_meta": {
+            "identifier": f"{coinHex} Payment",
+            "instruction": f"Scan to pay {amount} {coinHex}"
+        }
+        })
+        return paymentRequest
