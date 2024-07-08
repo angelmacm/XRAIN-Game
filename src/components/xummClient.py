52a6f97c733c81233e83affd53bfcba0067b79b9
ammacapagal <73205874+angelmacm@users.noreply.github.com>
2024-07-08 17:50:37 +0800

    add createSignInQR in xumm module

--- src/components/xummClient.py
+++ src/components/xummClient.py
@@ -6,3 +6,9 @@ class XummClient:
     def __init__(self):
         self.xummSdk = XummSdk(xummConfig['API_KEY'], xummConfig['API_SECRET'])
         
+    def createSignInQR(self):
+        signInInfo = self.xummSdk.payload.create(payload={
+            'TransactionType': "SignIn"
+            })
+         
+        return signInInfo.refs.qr_png
