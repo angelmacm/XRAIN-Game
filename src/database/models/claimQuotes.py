aec2514406dd2ff726fc6e9d50d1fde8a1ed7468
ammacapagal <73205874+angelmacm@users.noreply.github.com>
2024-07-12 23:56:34 +0800

    add option to set claimQuotes table name via config

--- src/database/models/claimQuotes.py
+++ src/database/models/claimQuotes.py
@@ -2,11 +2,13 @@ from sqlalchemy import Column, Integer, VARCHAR
 from sqlalchemy.orm import column_property
 from sqlalchemy.ext.declarative import declarative_base
 
+from components.config import dbConfig
+
 # Define the base class
 Base = declarative_base()
 
 class ClaimQuotes(Base):
-    __tablename__ = 'claimquotes'
+    __tablename__ = dbConfig['claim_quotes_table_name']
 
     quoteId = Column(Integer, index=True, primary_key=True)
     nftGroupName = column_property(Column('NFTGroupName', VARCHAR))
